6057d1fded7191d765418eacc3e8d2eecd7a492a
alisonarmstrong <alarmstrong15@gmail.com>
2021-03-04 10:04:05 -0700

    added prompts and arrays

---
+++ script.js
@@ -0,0 +1,37 @@
+var generateBtn = document.querySelector("#generate");
+
+// Assignment Code
+
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumber;
+var confirmLowerCase;
+var confirmUpperCase;
+
+
+
+function generatePassword() {
+  var confirmLength = prompt("How many characters will the password be? Enter a number between 8 and 128"));
+}
+
+var confirmSpecialCharacter = confirm("Click OK to include special characters");
+var confirmNumericCharacter = confirm("Click OK to include numeric characters");    
+var confirmLowerCase = confirm("Click OK to include lowercase characters");
+var confirmUpperCase = confirm("Click OK to confirm to include uppercase characters");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
